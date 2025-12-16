b5c308bd5d4e05cb42b7eeccb25706ff71e399d2
Dosyaa <dossymzhan.zhumabidenov@narxoz.kz>
2025-12-16 14:06:31 +0500

    Initial commit

---
+++ src/main/java/com/example/finalprojectrpo/FinalProjectRpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectrpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectRpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectRpoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalprojectrpo/FinalProjectRpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectrpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectRpoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
