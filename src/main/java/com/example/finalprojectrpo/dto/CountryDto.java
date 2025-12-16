29e4afc3d93cb258dcf1ff6587ad805731a1cbb3
Bekw69 <kadylbekb@gmail.com>
2025-12-16 15:04:16 +0500

    feature / add dto for models

---
+++ src/main/java/com/example/finalprojectrpo/dto/CountryDto.java
@@ -0,0 +1,14 @@
+package com.example.finalprojectrpo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CountryDto {
+    private Long id;
+    private String name;
+    private String code;
+}
